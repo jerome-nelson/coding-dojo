f3e5f7ec631900e2ad0da97926ea622b892c92e4
mmolka <maciej.molka@skelia.com>
2020-06-23 18:54:26 +0200

    feat: bfs first lesson

---
+++ packages/algorithms/src/bfs/bfs.test.ts
@@ -0,0 +1,70 @@
+// Input will be a adjacency list
+export const expected = [
+    [1,
+        [3, null],
+        [4, null]
+    ],
+    [2,
+        [5,
+            [6, null]
+        ]
+    ]
+];
+
+const mock = {
+    root: {
+        right: {
+            value: 2,
+            left: {
+                value: 5,
+                left: {
+                    value: 6
+                }
+            },
+            right: null
+        },
+        left: {
+            value: 1,
+            left: {
+                value: 3
+            },
+            right: {
+                value: 4
+            }
+        }
+    }
+}
+
+const breadFirst = (nodes: any): any => {
+
+    const output = {
+        root: {}
+    };
+
+
+    // 1. Iterate through array
+    // 2. Only take first child
+    // i. First child is ALWAYS the first item in the subarray
+    // i.e list[0][0] (will always be direct descendant)
+
+    (output.root as any).left = {
+        value: nodes[0][0]
+    };
+    (output.root as any).right = {
+        value: nodes[1][0]
+    }
+
+    return output;
+
+}
+
+// Output should be a multi-dimensional object
+
+describe("BFS", () => {
+    it("Creates a root", () => {
+        expect(breadFirst(expected).root).toBeTruthy()
+    });
+    // it("should create a tree-like structure representing the graphical list", () => {
+    //     expect(JSON.stringify(breadFirst(expected))).toEqual(JSON.stringify(mock));
+    // });
+});
